76fb02b086ad95dee2fd0cf198b2fc9ad21ed8f6
Nahomi Venegas Preciado <nahomi.venegas@alumnos.udg.mx>
2025-05-08 09:38:28 -0600

    Actualización del mensaje de bienvenida y mejoras visuales

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
+  
   standalone: false,
 })
 export class TabsPage implements OnInit {
